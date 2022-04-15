711f5a5fb135d4bf1316941daf73e12de4056619
Aleksei Krasnov <alexeykrasnov1989@gmail.com>
2022-04-15 10:57:26 +0300

    Add Random forest alrotithm

--- Eg_model.py
+++ Eg_model.py
@@ -12,12 +12,15 @@ from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
 from sklearn.svm import SVC
 import pickle
+import joblib
 from sklearn.svm import SVR
 import pymatgen as mg
 from pymatgen.core.composition import Composition
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 
+# Classification
 #read data
-DE_c = pd.read_excel('Training_Set.xlsx',sheet_name=0)
+DE_c = pd.read_excel('Training_Set.xlsx', sheet_name=0)
 array_c = DE_c.values
 X_c = array_c[:,3:139]
 Y_c = array_c[:,2]
@@ -29,12 +32,14 @@ scaler_c = preprocessing.StandardScaler().fit(X_train_c)
 X_train_c = scaler_c.transform(X_train_c)
 X_test_c = scaler_c.transform(X_test_c)
 #model construction
-classification=SVC(kernel='rbf',C=10**1.5, gamma= 0.01).fit(X_train_c, Y_train_c)
+# classification=SVC(kernel='rbf',C=10**1.5, gamma= 0.01).fit(X_train_c, Y_train_c)
+rf_classification = RandomForestClassifier(n_jobs=-1).fit(X_train_c, Y_train_c)
 #save model
-classification_model=pickle.dumps(classification)
+classification_model=pickle.dumps(rf_classification)
 
+# REgression
 #read data
-DE_r = pd.read_excel('Training_Set.xlsx',sheet_name=1)
+DE_r = pd.read_excel('Training_Set.xlsx', sheet_name=1)
 array_r = DE_r.values
 X_r = array_r[:,2:138]
 Y_r = array_r[:,1]
@@ -45,11 +50,12 @@ scaler_r = preprocessing.StandardScaler().fit(X_train_r)
 X_train_r = scaler_r.transform(X_train_r)
 X_test_r = scaler_r.transform(X_test_r)
 #model construction
-regression = SVR(kernel='rbf',C=10, epsilon=0.1, gamma= 0.01).fit(X_train_r, Y_train_r)
-predicted_Y1_r = regression.predict(X_train_r)
-predicted_Y_r = regression.predict(X_test_r)
+# regression = SVR(kernel='rbf',C=10, epsilon=0.1, gamma= 0.01).fit(X_train_r, Y_train_r)
+rf_regression = RandomForestRegressor(n_jobs=-1, random_state=101).fit(X_train_r, Y_train_r)
+predicted_Y1_r = rf_regression.predict(X_train_r)
+predicted_Y_r = rf_regression.predict(X_test_r)
 #save model
-regression_model=pickle.dumps(regression)
+regression_model=pickle.dumps(rf_regression)
 
 prediction = pd.read_excel('to_predict.xlsx')
 prediction.head()
@@ -58,7 +64,7 @@ prediction.dtypes
 class Vectorize_Formula:
 	def __init__(self):
 		elem_dict = pd.read_excel(r'elements.xlsx')
-		self.element_df = pd.DataFrame(elem_dict) 
+		self.element_df = pd.DataFrame(elem_dict)
 		self.element_df.set_index('Symbol',inplace=True)
 		self.column_names = []
 		for string in ['avg','diff','max','min']:
@@ -73,9 +79,9 @@ class Vectorize_Formula:
 			sum_feature = np.zeros(len(self.element_df.iloc[0]))
 			for key in fractional_composition:
 				try:
-					avg_feature += self.element_df.loc[key].values * fractional_composition[key]                  
+					avg_feature += self.element_df.loc[key].values * fractional_composition[key]
 					diff_feature = self.element_df.loc[list(fractional_composition.keys())].max()-self.element_df.loc[list(fractional_composition.keys())].min()
-				except Exception as e: 
+				except Exception as e:
 					print('The element:', key, 'from formula', formula,'is not currently supported in our database')
 					return np.array([np.nan]*len(self.element_df.iloc[0])*4)
 			max_feature = self.element_df.loc[list(fractional_composition.keys())].max()
