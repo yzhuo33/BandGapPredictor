8aba891f4d93caf89d273a29b54fa2de3e31b651
Aleksei Krasnov <alexeykrasnov1989@gmail.com>
2022-04-15 13:01:29 +0300

    Add Random forest alrotithm

--- Eg_model.py
+++ Eg_model.py
@@ -13,7 +13,6 @@ from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn import preprocessing
 from sklearn import metrics
-from sklearn.svm import SVC, SVR
 import pickle
 from pymatgen.core.composition import Composition
 import matplotlib.pyplot as plt
@@ -26,24 +25,21 @@ Y_c = array_c[:, 2]
 Y_c = Y_c.astype('int')
 
 # train/test split
-X_train_c, X_test_c, Y_train_c, Y_test_c = train_test_split(X_c, Y_c, test_size=0.1, random_state=15, shuffle=True)
+X_train_c, X_test_c, Y_train_c, Y_test_c = train_test_split(X_c, Y_c,  random_state=15, shuffle=True)
 
 # preprocessing
-scaler_c = preprocessing.StandardScaler().fit(X_train_c)
-X_train_c = scaler_c.transform(X_train_c)
-X_test_c = scaler_c.transform(X_test_c)
-
-# SVM model construction
-# classification=SVC(kernel='rbf',C=10**1.5, gamma= 0.01).fit(X_train_c, Y_train_c)
-# print("SVM classification accuracy:", classification.score(X_test_c,  Y_test_c))
+scaler_c = preprocessing.StandardScaler()
+X_train_c = scaler_c.fit_transform(X_train_c)
+X_test_c = scaler_c.fit_transform(X_test_c)
+# scaler_c = preprocessing.StandardScaler().fit(X_c)
+X_c = scaler_c.fit_transform(X_c)
 
 # Random forest model construction
-classification = RandomForestClassifier(n_jobs=-1, n_estimators=50, max_features=12,
-                                        random_state=100).fit(X_train_c, Y_train_c)
+classification_for_eval = RandomForestClassifier(n_jobs=-1, random_state=100).fit(X_train_c, Y_train_c)
 
 # Estimation of classification model performance
-y_pred_c = classification.predict(X_test_c)
-print("Random forest classification accuracy:", classification.score(X_test_c,  Y_test_c))
+y_pred_c = classification_for_eval.predict(X_test_c)
+print("Random forest classification accuracy:", classification_for_eval.score(X_test_c,  Y_test_c))
 print("Random forest classification precision:", metrics.precision_score(Y_test_c, y_pred_c))
 print("Random forest classification recall:", metrics.recall_score(Y_test_c, y_pred_c))
 print("Random forest classification F1 score:", metrics.f1_score(Y_test_c, y_pred_c))
@@ -67,8 +63,9 @@ print("Random forest classification F1 score:", metrics.f1_score(Y_test_c, y_pre
 # plt.xlabel('max_features')
 # plt.ylabel('f1_score')
 # plt.savefig('Elbow Graph for classification_max_features.png')
-
+# Final  model
 # save model
+classification = RandomForestClassifier(n_jobs=-1, random_state=100, n_estimators=100, max_features=20).fit(X_c, Y_c)
 classification_model = pickle.dumps(classification)
 
 # Regression
@@ -78,31 +75,37 @@ X_r = array_r[:, 2:138]
 Y_r = array_r[:, 1]
 
 # train/test split
-X_train_r, X_test_r, Y_train_r, Y_test_r = train_test_split(X_r, Y_r, test_size=0.1, random_state=15, shuffle=True)
+X_train_r, X_test_r, Y_train_r, Y_test_r = train_test_split(X_r, Y_r, random_state=15, shuffle=True)
 
 # preprocessing
-scaler_r = preprocessing.StandardScaler().fit(X_train_r)
-X_train_r = scaler_r.transform(X_train_r)
-X_test_r = scaler_r.transform(X_test_r)
-
-# SVM model construction
-# regression = SVR(kernel='rbf',C=10, epsilon=0.1, gamma= 0.01).fit(X_train_r, Y_train_r)
-# print("SVM regression accuracy:", regression.score(X_test_r,  Y_test_r))
+scaler_r = preprocessing.StandardScaler()
+X_train_r = scaler_r.fit_transform(X_train_r)
+X_test_r = scaler_r.fit_transform(X_test_r)
+# scaler_r = preprocessing.StandardScaler().fit(X_r)
+X_r = scaler_r.fit_transform(X_r)
 
 # Random forest model construction
-regression = RandomForestRegressor(n_jobs=-1, n_estimators=100, max_features=25,
-                                   random_state=100, ).fit(X_train_r, Y_train_r)
-# predicted_Y1_r = regression.predict(X_train_r)
+regression_eval = RandomForestRegressor(n_jobs=-1, random_state=100).fit(X_train_r, Y_train_r)
+predicted_Y1_r = regression_eval.predict(X_train_r)
 
 # Estimation of regression model performance
-y_pred_r = regression.predict(X_test_r)
-print("Random forest regression accuracy:", regression.score(X_test_r,  Y_test_r))
+y_pred_r = regression_eval.predict(X_test_r)
+print("Random forest regression accuracy:", regression_eval.score(X_test_r,  Y_test_r))
 print('Mean Absolute Error (MAE):', metrics.mean_absolute_error(Y_test_r, y_pred_r))
 print('Mean Squared Error (MSE):', metrics.mean_squared_error(Y_test_r, y_pred_r))
 print('Root Mean Squared Error (RMSE):', metrics.mean_squared_error(Y_test_r, y_pred_r, squared=False))
 print('Explained Variance Score:', metrics.explained_variance_score(Y_test_r, y_pred_r))
 print('Mean Squared Log Error:', metrics.mean_squared_log_error(Y_test_r, y_pred_r))
 print('Median Absolute Error:', metrics.median_absolute_error(Y_test_r, y_pred_r))
+
+# Grid Search to compare the performance of a random forest with different numbers of trees.
+# param_grid = {
+#     'n_estimators': [i for i in range(50, 301, 50)],
+#     'max_features': [i for i in range(1, 101, 5)]
+# }
+# gs = GridSearchCV(regression, param_grid, cv=5)
+# gs.fit(X_train_r, Y_train_r)
+# print("best params for regression:", gs.best_params_)
 
 # Elbow Graph to define optimal number of trees in forest
 # n_estimators = [i for i in range(10, 301, 50)]
@@ -117,7 +120,9 @@ print('Median Absolute Error:', metrics.median_absolute_error(Y_test_r, y_pred_r
 # plt.ylabel('accuracy')
 # plt.savefig('Elbow Graph for regression_max_features.png')
 
-# # save model
+# Final model
+regression = RandomForestRegressor(n_jobs=-1, random_state=100, n_estimators=150, max_features=25).fit(X_r, Y_r)
+# save model
 regression_model = pickle.dumps(regression)
 
 prediction = pd.read_excel('to_predict.xlsx')
