088a89f5be44d6e3975bd90c02d7a6634eac9ad6
Aleksei Krasnov <alexeykrasnov1989@gmail.com>
2022-04-15 12:23:51 +0300

    Add Random forest alrotithm

--- Eg_model.py
+++ Eg_model.py
@@ -10,13 +10,13 @@ Modified on Thu Feb 3 10:06:00 2022
 import pandas as pd
 import numpy as np
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn import preprocessing
 from sklearn import metrics
 from sklearn.svm import SVC, SVR
 import pickle
 from pymatgen.core.composition import Composition
-
+import matplotlib.pyplot as plt
 
 # Classification
 DE_c = pd.read_excel('Training_Set.xlsx', sheet_name=0)
@@ -38,14 +38,35 @@ X_test_c = scaler_c.transform(X_test_c)
 # print("SVM classification accuracy:", classification.score(X_test_c,  Y_test_c))
 
 # Random forest model construction
-classification = RandomForestClassifier(n_jobs=-1).fit(X_train_c, Y_train_c)
+classification = RandomForestClassifier(n_jobs=-1, n_estimators=50, max_features=12,
+                                        random_state=100).fit(X_train_c, Y_train_c)
 
 # Estimation of classification model performance
-y_pred = classification.predict(X_test_c)
+y_pred_c = classification.predict(X_test_c)
 print("Random forest classification accuracy:", classification.score(X_test_c,  Y_test_c))
-print("Random forest classification precision:", metrics.precision_score(Y_test_c, y_pred))
-print("Random forest classification recall:", metrics.recall_score(Y_test_c, y_pred))
-print("Random forest classification F1 score:", metrics.f1_score(Y_test_c, y_pred))
+print("Random forest classification precision:", metrics.precision_score(Y_test_c, y_pred_c))
+print("Random forest classification recall:", metrics.recall_score(Y_test_c, y_pred_c))
+print("Random forest classification F1 score:", metrics.f1_score(Y_test_c, y_pred_c))
+
+# Grid Search to compare the performance of a random forest with different numbers of trees.
+# param_grid = {
+#     'n_estimators': [i for i in range(200, 301, 10)],
+# }
+# gs = GridSearchCV(classification, param_grid, cv=5, scoring='f1')
+# gs.fit(X_train_c, Y_train_c)
+# print("best params for classification:", gs.best_params_)
+
+# Elbow Graph to define optimal number of trees in forest
+# n_estimators = [i for i in range(10, 301, 10)]
+# max_features = [i for i in range(1, 101, 5)]
+# param_grid = {'max_features': max_features}
+# gs = GridSearchCV(classification, param_grid, cv=5, scoring='f1')
+# gs.fit(X_train_c, Y_train_c)
+# scores = gs.cv_results_['mean_test_score']
+# plt.plot(max_features, scores)
+# plt.xlabel('max_features')
+# plt.ylabel('f1_score')
+# plt.savefig('Elbow Graph for classification_max_features.png')
 
 # save model
 classification_model = pickle.dumps(classification)
@@ -69,20 +90,34 @@ X_test_r = scaler_r.transform(X_test_r)
 # print("SVM regression accuracy:", regression.score(X_test_r,  Y_test_r))
 
 # Random forest model construction
-regression = RandomForestRegressor(n_jobs=-1, random_state=101).fit(X_train_r, Y_train_r)
-predicted_Y1_r = regression.predict(X_train_r)
-predicted_Y_r = regression.predict(X_test_r)
+regression = RandomForestRegressor(n_jobs=-1, n_estimators=100, max_features=25,
+                                   random_state=100, ).fit(X_train_r, Y_train_r)
+# predicted_Y1_r = regression.predict(X_train_r)
 
 # Estimation of regression model performance
+y_pred_r = regression.predict(X_test_r)
 print("Random forest regression accuracy:", regression.score(X_test_r,  Y_test_r))
-print('Mean Absolute Error (MAE):', metrics.mean_absolute_error(Y_test_r, predicted_Y_r))
-print('Mean Squared Error (MSE):', metrics.mean_squared_error(Y_test_r, predicted_Y_r))
-print('Root Mean Squared Error (RMSE):', metrics.mean_squared_error(Y_test_r, predicted_Y_r, squared=False))
-print('Explained Variance Score:', metrics.explained_variance_score(Y_test_r, predicted_Y_r))
-print('Mean Squared Log Error:', metrics.mean_squared_log_error(Y_test_r, predicted_Y_r))
-print('Median Absolute Error:', metrics.median_absolute_error(Y_test_r, predicted_Y_r))
+print('Mean Absolute Error (MAE):', metrics.mean_absolute_error(Y_test_r, y_pred_r))
+print('Mean Squared Error (MSE):', metrics.mean_squared_error(Y_test_r, y_pred_r))
+print('Root Mean Squared Error (RMSE):', metrics.mean_squared_error(Y_test_r, y_pred_r, squared=False))
+print('Explained Variance Score:', metrics.explained_variance_score(Y_test_r, y_pred_r))
+print('Mean Squared Log Error:', metrics.mean_squared_log_error(Y_test_r, y_pred_r))
+print('Median Absolute Error:', metrics.median_absolute_error(Y_test_r, y_pred_r))
 
-# save model
+# Elbow Graph to define optimal number of trees in forest
+# n_estimators = [i for i in range(10, 301, 50)]
+# max_features = [i for i in range(1, 101, 5)]
+# param_grid = {'max_features': max_features}
+# gs = GridSearchCV(regression, param_grid, cv=5)
+# gs.fit(X_train_r, Y_train_r)
+# scores = gs.cv_results_['mean_test_score']
+# # plt.plot(n_estimators, scores)
+# plt.plot(max_features, scores)
+# plt.xlabel('max_features')
+# plt.ylabel('accuracy')
+# plt.savefig('Elbow Graph for regression_max_features.png')
+
+# # save model
 regression_model = pickle.dumps(regression)
 
 prediction = pd.read_excel('to_predict.xlsx')
@@ -91,44 +126,44 @@ prediction.dtypes
 
 
 class VectorizeFormula:
-	def __init__(self):
-		elem_dict = pd.read_excel(r'elements.xlsx')
-		self.element_df = pd.DataFrame(elem_dict)
-		self.element_df.set_index('Symbol', inplace=True)
-		self.column_names = []
-		for string in ['avg', 'diff', 'max', 'min']:
-			for column_name in self.element_df.columns.values:
-				self.column_names.append(string+'_'+column_name)
+    def __init__(self):
+        elem_dict = pd.read_excel(r'elements.xlsx')
+        self.element_df = pd.DataFrame(elem_dict)
+        self.element_df.set_index('Symbol', inplace=True)
+        self.column_names = []
+        for string in ['avg', 'diff', 'max', 'min']:
+            for column_name in self.element_df.columns.values:
+                self.column_names.append(string+'_'+column_name)
 
-	def get_features(self, formula):
-		try:
-			fractional_composition = Composition(formula).fractional_composition.as_dict()
-			element_composition = Composition(formula).element_composition.as_dict()
-			avg_feature = np.zeros(len(self.element_df.iloc[0]))
-			sum_feature = np.zeros(len(self.element_df.iloc[0]))
-			for key in fractional_composition:
-				try:
-					avg_feature += self.element_df.loc[key].values * fractional_composition[key]
-					diff_feature = self.element_df.loc[list(fractional_composition.keys())].max()-self.element_df.loc[list(fractional_composition.keys())].min()
-				except Exception as e:
-					print('The element:', key, 'from formula', formula, 'is not currently supported in our database')
-					return np.array([np.nan]*len(self.element_df.iloc[0])*4)
-			max_feature = self.element_df.loc[list(fractional_composition.keys())].max()
-			min_feature = self.element_df.loc[list(fractional_composition.keys())].min()
+    def get_features(self, formula):
+        try:
+            fractional_composition = Composition(formula).fractional_composition.as_dict()
+            element_composition = Composition(formula).element_composition.as_dict()
+            avg_feature = np.zeros(len(self.element_df.iloc[0]))
+            sum_feature = np.zeros(len(self.element_df.iloc[0]))
+            for key in fractional_composition:
+                try:
+                    avg_feature += self.element_df.loc[key].values * fractional_composition[key]
+                    diff_feature = self.element_df.loc[list(fractional_composition.keys())].max()-self.element_df.loc[list(fractional_composition.keys())].min()
+                except Exception as e:
+                    print('The element:', key, 'from formula', formula, 'is not currently supported in our database')
+                    return np.array([np.nan]*len(self.element_df.iloc[0])*4)
+            max_feature = self.element_df.loc[list(fractional_composition.keys())].max()
+            min_feature = self.element_df.loc[list(fractional_composition.keys())].min()
 
-			features = pd.DataFrame(np.concatenate([avg_feature, diff_feature, np.array(max_feature), np.array(min_feature)]))
-			features = np.concatenate([avg_feature, diff_feature, np.array(max_feature), np.array(min_feature)])
-			return features.transpose()
-		except:
-			print('There was an error with the Formula: ' + formula + ', this is a general exception with an unkown error')
-			return [np.nan]*len(self.element_df.iloc[0])*4
+            features = pd.DataFrame(np.concatenate([avg_feature, diff_feature, np.array(max_feature), np.array(min_feature)]))
+            features = np.concatenate([avg_feature, diff_feature, np.array(max_feature), np.array(min_feature)])
+            return features.transpose()
+        except:
+            print('There was an error with the Formula: ' + formula + ', this is a general exception with an unkown error')
+            return [np.nan]*len(self.element_df.iloc[0])*4
 
 
 gf = VectorizeFormula()
 features = []
 targets = []
 for formula in prediction['Composition']:
-	features.append(gf.get_features(formula))
+    features.append(gf.get_features(formula))
 X = pd.DataFrame(features, columns=gf.column_names)
 pd.set_option('display.max_columns', None)
 
@@ -140,10 +175,10 @@ pred_r = pickle.loads(regression_model)
 result_r = pred_r.predict(X_r)
 result = []
 for i in range(len(result_c)):
-	if result_c[i] == 1:
-		result.append(result_r[i])
-	else:
-		result.append(result_c[i])
+    if result_c[i] == 1:
+        result.append(result_r[i])
+    else:
+        result.append(result_c[i])
 result = np.around(result, decimals=2)
 
 composition = pd.read_excel('to_predict.xlsx', sheet_name='Sheet1', usecols="A")
